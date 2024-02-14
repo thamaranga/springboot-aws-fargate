0bd8a83ef91888a8fe698ae57bc558544d3bd3eb
unknown <hasithathamaranga@gmail.com>
2024-02-14 11:59:25 +0800

    first commit

---
+++ src/main/java/com/hasithat/springbootawsfargate/SpringbootAwsFargateApplication.java
@@ -0,0 +1,13 @@
+package com.hasithat.springbootawsfargate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootAwsFargateApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootAwsFargateApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/hasithat/springbootawsfargate/SpringbootAwsFargateApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hasithat.springbootawsfargate;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootAwsFargateApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
